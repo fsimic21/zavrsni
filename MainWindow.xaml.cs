ddd657b0d38847bfc393275c9e0ddc2b16402a6f
Frano <fsimic21@student.foi.hr>
2024-07-19 11:32:35 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using DataAccessLayer;
+using ServiceLayer;
+using System.Windows;
+
+namespace PresentationLayer
+{
+    public partial class MainWindow : Window
+    {
+        CarService service;
+        CarRepository repository;
+        public MainWindow()
+        {
+            InitializeComponent();
+            repository = new CarRepository();
+            service = new CarService(repository);
+        }
+
+        private void HomeScreen_Click(object sender, RoutedEventArgs e)
+        {
+            UcHomeScreen screen = new UcHomeScreen();
+            MainContent.Content = screen;
+        }
+
+        private void Camera_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Image_Click(object sender, RoutedEventArgs e) {
+            UcImage screen = new UcImage(service);
+            MainContent.Content = screen;
+        }
+    }
+}
---
+++ UcImage.xaml.cs
@@ -0,0 +1,194 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Microsoft.Win32;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Quantization;
+using System;
+using System.Drawing;
+using Patagames.Ocr;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Color = System.Drawing.Color;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+using System.IO;
+using System.Text.RegularExpressions;
+using ServiceLayer;
+
+namespace PresentationLayer {
+    public partial class UcImage : UserControl {
+
+        CarService service;
+
+        public UcImage(CarService service) {
+            this.service = service;
+            InitializeComponent();
+            
+        }
+
+        private void LoadImageButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) {
+                string filePath = openFileDialog.FileName;
+                DisplayImage(filePath);
+                GrayImageDisplay(filePath);
+                DetectCarColour(filePath);
+            }
+        }
+
+        private void DisplayImage(string filePath) {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(filePath);
+            bitmap.EndInit();
+            LoadedImage.Source = bitmap;
+        }
+
+        private void GrayImageDisplay(string filePath) {
+            Mat img = CvInvoke.Imread(filePath, ImreadModes.Color);
+            Mat gray = ProcessImage(img);
+            BitmapImage bitmapImage = ConvertMatToBitmapImage(gray);
+            GrayImage.Source = bitmapImage;
+            GetLicensePlate(BitmapImage2Bitmap(bitmapImage));
+        }
+
+        private void GetLicensePlate(Bitmap image) {
+            using (var obj = OcrApi.Create()) {
+                obj.Init(Patagames.Ocr.Enums.Languages.English);
+                string text = obj.GetTextFromImage(image);
+
+                string filteredText = Regex.Replace(text, @"[^a-zA-Z0-9]", string.Empty);
+
+                LicenceTextBox.Text = filteredText;
+            }
+        }
+
+
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage) {
+
+            using (MemoryStream outStream = new MemoryStream()) {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                Bitmap bitmap = new Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+
+
+        private BitmapImage ConvertMatToBitmapImage(Mat mat) {
+            using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream()) {
+                Bitmap bitmap = mat.ToBitmap();
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                memoryStream.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+
+        private void DetectCarColour(string filePath) {
+            Rgb24 dominantColor = GetDominantColorUsingImageSharp(filePath);
+            Color color = Color.FromArgb(dominantColor.R, dominantColor.G, dominantColor.B);
+            ColorTextBox.Text = ColorTranslator.ToHtml(color);
+            ColorTextBox.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(dominantColor.R, dominantColor.G, dominantColor.B));
+        }
+
+        private Rgb24 GetDominantColorUsingImageSharp(string filePath) {
+            using (var image = SixLabors.ImageSharp.Image.Load<Rgb24>(filePath)) {
+                image.Mutate(x => x
+                    .Resize(new ResizeOptions() { Sampler = KnownResamplers.NearestNeighbor, Size = new SixLabors.ImageSharp.Size(100, 0) })
+                    .Quantize(new OctreeQuantizer(new QuantizerOptions { Dither = null })));
+
+                return image[0, 0];
+            }
+        }
+
+
+
+        public Mat ProcessImage(Mat img) {
+            using (UMat gray = new UMat())
+            using (UMat cannyEdges = new UMat()) {
+                CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
+                CvInvoke.GaussianBlur(gray, gray, new Size(3, 3), 1);
+
+                double cannyThreshold = 180.0;
+                double cannyThresholdLinking = 120.0;
+                CvInvoke.Canny(gray, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+                List<RotatedRect> boxList = new List<RotatedRect>(); 
+                using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint()) {
+                    CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                    int count = contours.Size;
+                    for (int i = 0; i < count; i++) {
+                        using (VectorOfPoint contour = contours[i])
+                        using (VectorOfPoint approxContour = new VectorOfPoint()) {
+                            CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                            if (CvInvoke.ContourArea(approxContour, false) > 250) {
+                                if (approxContour.Size == 4) {
+                                    bool isRectangle = true;
+                                    Point[] pts = approxContour.ToArray();
+                                    LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+                                    for (int j = 0; j < edges.Length; j++) {
+                                        double angle = Math.Abs(edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                        if (angle < 80 || angle > 100) {
+                                            isRectangle = false;
+                                            break;
+                                        }
+                                    }
+
+                                    if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                RotatedRect largestBox = new RotatedRect();
+                double largestArea = 0;
+                foreach (RotatedRect box in boxList) {
+                    double area = box.Size.Width * box.Size.Height;
+                    if (area > largestArea) {
+                        largestArea = area;
+                        largestBox = box;
+                    }
+                }
+                Mat result = img;
+                if (largestArea > 0) {
+                    PointF[] vertices = largestBox.GetVertices();
+                    Point[] points = Array.ConvertAll(vertices, Point.Round);
+                    Rectangle boundingRect = CvInvoke.BoundingRectangle(points);
+
+                    result = new Mat(img, boundingRect);
+                }
+
+                if (result.Width > 499) {
+                    double scaleFactor = 499.0 / result.Width;
+                    Size newSize = new Size(499, (int)(result.Height * scaleFactor));
+                    CvInvoke.Resize(result, result, newSize);
+                }
+
+                return result;
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
